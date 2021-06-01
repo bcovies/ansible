416f123126ea2d2550c4eb775b3787d826db93e6
Bruno Souto <bcovies@gmail.com>
2021-06-01 13:09:36 -0300

    Fix error dir_repositoio

---
+++ centos_8_stream/install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+sudo yum update -y
+sudo yum install -y git
+git clone git@github.com:bcovies/ansible.git
+
+sudo mkdir /mnt/{hdd,ssd}
+sudo mount /dev/sda /mnt/ssd
+sudo mount /dev/sdb /mnt/hdd
+
+
+echo "/dev/sdb /mnt/hdd auto defaults 0 0 " >> /etc/fstab
+echo "/dev/sda /mnt/ssd auto defaults 0 0 " >> /etc/fstab
+
+
+
+sudo yum install epel-release -y
+sudo yum install ansible -y
