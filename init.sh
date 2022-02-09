0a7982d53e62da3f1cb81b116c54cc1505af77e5
Bruno Souto <bcovies@gmail.com>
2022-02-08 22:51:48 -0300

    Adicionado confd para facilitar a criacao dos templates

---
+++ init.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+rename_files_tmp(){
+    mv forward.domain.sufix 
+}
+
+create_path_tmp(){
+    mkdir -p /tmp/confd/named/
+}
+
+create_env(){
+    DOMAIN_SUFIX=
+    DNS_HOST_IP=
+    IP_MACHINE_1=
+    IP_MACHINE_2=
+    IP_MACHINE_3=
+    IP_MACHINE_4=
+    IP_MACHINE_5=
+    IP_MACHINE_6=
+    NAME_MACHINE_1=
+    NAME_MACHINE_2=
+    NAME_MACHINE_3=
+    NAME_MACHINE_4=
+    NAME_MACHINE_5=
+    NAME_MACHINE_6=
+}
