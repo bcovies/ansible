db97c490a96d4d471b4bdc9ed3cd9acb089d6605
Bruno Souto <bcovies@gmail.com>
2022-02-09 21:28:14 -0300

    Adicionado script funcional para automacao dos templates

--- init.sh
+++ init.sh
@@ -1,26 +1,94 @@
 #!/bin/bash
 
-rename_files_tmp(){
-    mv forward.domain.sufix 
+PROJECTS_TEMPLATES=( "ansible" "named" "netplan" "nginx" "ssl")
+CONFD_BIN_FILE=confd-0.16.0-linux-amd64
+PATH_TMP_FILES=/tmp/confd
+
+get_local_pwd(){
+    CURRENT_PATH=$(pwd)
+    echo "----------------------------------------------------------------------------"
+    echo "Your current path is: ${CURRENT_PATH}"
 }
 
-create_path_tmp(){
-    mkdir -p /tmp/confd/named/
+create_templates_path_tmp(){
+    echo "----------------------------------------------------------------------------"
+    echo "Creating all /tmp/ folders to confd templates..."
+    for local_folder_name in ${PROJECTS_TEMPLATES[@]}; do
+        mkdir -p ${PATH_TMP_FILES}/${local_folder_name}
+        if [[ -d ${PATH_TMP_FILES}/${local_folder_name} ]]; then
+            echo "----------------------------------------------------------------------------"
+            echo "OK ${PATH_TMP_FILES}/${local_folder_name}"
+        else
+            echo "----------------------------------------------------------------------------"
+            echo "ERROR: cloud not create ${PATH_TMP_FILES}/${local_folder_name}"
+            exit 1
+        fi
+    done
 }
 
-create_env(){
-    DOMAIN_SUFIX=
-    DNS_HOST_IP=
-    IP_MACHINE_1=
-    IP_MACHINE_2=
-    IP_MACHINE_3=
-    IP_MACHINE_4=
-    IP_MACHINE_5=
-    IP_MACHINE_6=
-    NAME_MACHINE_1=
-    NAME_MACHINE_2=
-    NAME_MACHINE_3=
-    NAME_MACHINE_4=
-    NAME_MACHINE_5=
-    NAME_MACHINE_6=
+create_templates_confd(){
+    if [[ ! -z ${CURRENT_PATH} ]]; then
+        chmod +x ${CURRENT_PATH}/${CONFD_BIN_FILE}
+        echo "----------------------------------------------------------------------------"
+        for local_folder_name in ${PROJECTS_TEMPLATES[@]}; do
+            echo "CURRENT TEMPLATE: ${local_folder_name}"
+            echo
+            env $(cat .env | grep ^[A-Z] | xargs) \
+                ${CURRENT_PATH}/${CONFD_BIN_FILE} -onetime \
+                    -confdir ${CURRENT_PATH}/templates/${local_folder_name} -backend env
+            echo "----------------------------------------------------------------------------"
+        done
+    else
+        echo "----------------------------------------------------------------------------"
+        echo "ERROR: Var CURRENT_PATH is empty, checkout this script!"
+    fi
 }
+
+rename_named_files(){
+    source .env
+
+    if [[ -f ${PATH_TMP_FILES}/named/forward.domain.sufix ]]; then
+        mv ${PATH_TMP_FILES}/named/forward.domain.sufix \
+            ${PATH_TMP_FILES}/named/forward.${DOMAIN_SUFIX}
+    else
+        echo "----------------------------------------------------------------------------"
+        echo "ERROR: ${PATH_TMP_FILES}/named/forward.domain.sufix DO NOT EXISTS!"
+        exit 1
+    fi
+
+    if [[ -f ${PATH_TMP_FILES}/named/reverse.domain.sufix ]]; then
+        mv ${PATH_TMP_FILES}/named/reverse.domain.sufix \
+            ${PATH_TMP_FILES}/named/reverse.${DOMAIN_SUFIX}
+    else
+        echo "----------------------------------------------------------------------------"
+        echo "ERROR: ${PATH_TMP_FILES}/named/reverse.domain.sufix DO NOT EXISTS!"
+        exit 1
+    fi
+}
+
+copy_files_tmp_folder(){
+    for local_folder_name in ${PROJECTS_TEMPLATES[@]}; do
+        echo "COPYING CURRENT TEMPLATE: ${local_folder_name}"
+        echo
+        if [[ ${local_folder_name} == 'ansible' ]]; then
+            echo "${PATH_TMP_FILES}/${local_folder_name} --> ${CURRENT_PATH}/ubuntu/group_vars/"
+            cp -a ${PATH_TMP_FILES}/${local_folder_name}/* ${CURRENT_PATH}/ubuntu/group_vars/
+        else
+            echo "${PATH_TMP_FILES}/${local_folder_name} --> ${CURRENT_PATH}/ubuntu/roles/${local_folder_name}/files/"
+            cp -a ${PATH_TMP_FILES}/${local_folder_name}/* ${CURRENT_PATH}/ubuntu/roles/${local_folder_name}/files/
+        fi
+        echo "----------------------------------------------------------------------------"
+    done
+}
+
+
+
+init(){
+    create_templates_path_tmp
+    get_local_pwd
+    create_templates_confd
+    rename_named_files
+    copy_files_tmp_folder
+}
+
+init
